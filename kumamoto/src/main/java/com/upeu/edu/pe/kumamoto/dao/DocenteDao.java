b81b9d1b167a168fcb7dfc0730c3e548416c6686
Rubenchuquivilca <38230003+Rubenchuquivilca@users.noreply.github.com>
2022-02-18 19:12:50 -0500

    www

--- kumamoto/src/main/java/com/upeu/edu/pe/kumamoto/dao/DocenteDao.java
+++ kumamoto/src/main/java/com/upeu/edu/pe/kumamoto/dao/DocenteDao.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.upeu.edu.pe.kumamoto.entity.Docente;
 
 public interface DocenteDao extends CrudRepository<Docente, Long>{
-
+	
+	
 }
